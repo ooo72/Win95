bbc0466da203585afcc44657631a042778555e50
ooo72 <hussamalshehri111@gmail.com>
2025-03-12 21:14:59 +0300

    Update win95.js

--- js/win95.js
+++ js/win95.js
@@ -2,10 +2,11 @@ Dos(document.getElementById("dosbox"), {
   w: 1054,
   h: 768,
   wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js"
-}).ready((fs, main) => {
-    main(["assets/win/AUTOEXEC.BAT"]);
+}).ready(async (fs, main) => {
+  await fs.extract("https://filebin.net/zdzd2938391win95/WIN.ZIP");
+  main(["AUTOEXEC.BAT"]);
 });
 setInterval(() => {
   document.title = "Win95";
 }, 100);
-console.log(window.location.href)
+console.log(window.location.href);
