125460fba37ce6381ad8aa4614a4a93eff2aa5be
ooo72 <hussamalshehri111@gmail.com>
2025-03-12 21:34:21 +0300

    Update win95.js

--- js/win95.js
+++ js/win95.js
@@ -3,10 +3,24 @@ Dos(document.getElementById("dosbox"), {
   h: 768,
   wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js"
 }).ready(async (fs, main) => {
-  await fs.extract("https://dn790004.ca.archive.org/0/items/win95_in_dosbox/package.zip");
-  main(["AUTOEXEC.BAT"]);
+  try {
+    const archiveUrl = "https://archive.org/download/win95_in_dosbox/package.zip";
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/" + archiveUrl;
+    const zipResponse = await fetch(proxyUrl);
+    
+    if (!zipResponse.ok) {
+      throw new Error(`Failed to download Win95: ${zipResponse.statusText}`);
+    }
+    const zipArrayBuffer = await zipResponse.arrayBuffer();
+    await fs.extract(new Uint8Array(zipArrayBuffer));
+    main(["AUTOEXEC.BAT"]);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
+
 setInterval(() => {
   document.title = "Win95";
 }, 100);
+
 console.log(window.location.href);
