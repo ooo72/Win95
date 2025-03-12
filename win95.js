adbfb7ae46193ae896991d189a8f33260423ecec
ooo72 <hussamalshehri111@gmail.com>
2025-03-12 14:22:52 +0300

    Add files via upload

---
+++ win95.js
@@ -0,0 +1,13 @@
+Dos(document.getElementById("dosbox"), {
+  w: 1054,
+  h: 768,
+  wdosboxUrl: "https://js-dos.com/6.22/current/wdosbox.js"
+}).ready((fs, main) => {
+  fs.extract("assets/WIN.ZIP").then(() => {
+    main(["AUTOEXEC.BAT"]);
+  });
+});
+setInterval(() => {
+  document.title = "Win95";
+}, 100);
+console.log(window.location.href)
